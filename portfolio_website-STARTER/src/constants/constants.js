3271223bc85913e070343d2f4357007218f11b3f
zahid <zahid.hassan@city.ac.uk>
2025-09-12 21:10:39 +0100

    Initial commit

--- portfolio_website-STARTER/src/constants/constants.js
+++ portfolio_website-STARTER/src/constants/constants.js
@@ -7,7 +7,7 @@ export const projects = [
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
-  },
+  },dsf
   {
     title: 'E-Commerce',
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
