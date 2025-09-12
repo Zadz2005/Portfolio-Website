652913013377e4604179f181398f0e7be5d0dce0
zahid <zahid.hassan@city.ac.uk>
2025-09-12 23:08:21 +0100

    header footer accomplishments made

--- portfolio_website-STARTER/src/components/Header/Header.js
+++ portfolio_website-STARTER/src/components/Header/Header.js
@@ -6,9 +6,43 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+        <Link href="/" passHref legacyBehavior>
+          <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
+            <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          </a>
+        </Link>
+    </Div1>
+    <Div2>
+      <li>
+          <Link href="#projects" passHref legacyBehavior>
+            <NavLink>Projects</NavLink>
+          </Link>
+      </li>
+      <li>
+          <Link href="#tech" passHref legacyBehavior>
+            <NavLink>Technologies</NavLink>
+          </Link>
+      </li>        
+      <li>
+          <Link href="#about" passHref legacyBehavior>
+            <NavLink>About</NavLink>
+          </Link>
+      </li>        
+    </Div2>
+      <Div3>
+        <SocialIcons href="https://google.com">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://google.com">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://google.com">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </Div3>
+    </Container>
 );
 
 export default Header;
